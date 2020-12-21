5946393d67f98ab8a59483ecd6bae884437c2b07
Daniel Benzion <daniel.z.benzion@gmail.com>
2020-12-21 01:09:05 -0500

    SleepIn

--- Warmup1/src/com/codingbat/SleepIn.java
+++ Warmup1/src/com/codingbat/SleepIn.java
@@ -2,4 +2,14 @@ package com.codingbat;
 
 public class SleepIn {
 
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        return vacation || !weekday;
+    }
+
+    public static void main(String[] args) {
+        SleepIn sleepIn = new SleepIn();
+        System.out.println("Expected true -> returned:" + sleepIn.sleepIn(true,true));
+        System.out.println("Expected false -> returned:" + sleepIn.sleepIn(true,false));
+        System.out.println("Expected true -> returned:" + sleepIn.sleepIn(false, false));
+    }
 }
