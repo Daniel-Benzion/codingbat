75f0512eabd4be31c45e2f8b5e90205cd58b8fc3
Daniel Benzion <daniel.z.benzion@gmail.com>
2020-12-28 12:21:46 -0500

    firstHalf

---
+++ String1/src/com/codingbat/FirstHalf.java
@@ -0,0 +1,25 @@
+package com.codingbat;
+
+/*
+Given a string of even length, return the first half. So the string "WooHoo" yields "Woo".
+
+
+firstHalf("WooHoo") → "Woo"
+firstHalf("HelloThere") → "Hello"
+firstHalf("abcdef") → "abc"
+ */
+
+public class FirstHalf {
+
+    public String firstHalf(String str) {
+        return str.substring(0, str.length() / 2);
+    }
+
+    public static void main(String[] args) {
+
+        FirstHalf firstHalf = new FirstHalf();
+        System.out.println("Expected Woo -> returned: " + firstHalf.firstHalf("WooHoo"));
+        System.out.println("Expected Hello -> returned: " + firstHalf.firstHalf("HelloThere"));
+        System.out.println("Expected abc -> returned: " + firstHalf.firstHalf("abcdef"));
+    }
+}
