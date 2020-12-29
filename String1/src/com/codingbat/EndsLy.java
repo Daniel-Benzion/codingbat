b79388163eda2e24c8cf1116e7f639032ba84684
Daniel Benzion <daniel.z.benzion@gmail.com>
2020-12-28 22:31:08 -0500

    endsLy

---
+++ String1/src/com/codingbat/EndsLy.java
@@ -0,0 +1,23 @@
+package com.codingbat;
+
+/*
+Given a string, return true if it ends in "ly".
+
+
+endsLy("oddly") → true
+endsLy("y") → false
+endsLy("oddy") → false
+ */
+
+public class EndsLy {
+
+    public boolean endsLy(String str) {
+        if (str.length() < 2) {
+            return false;
+        }
+        if (str.substring(str.length() - 2).equals("ly")) {
+            return true;
+        }
+        return false;
+    }
+}
