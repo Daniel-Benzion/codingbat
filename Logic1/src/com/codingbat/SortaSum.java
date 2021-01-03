efc6a92fb6abb5c4e9151fcd3df649f7f2888fe2
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-01-03 11:25:34 -0500

    sortaSum

---
+++ Logic1/src/com/codingbat/SortaSum.java
@@ -0,0 +1,20 @@
+package com.codingbat;
+
+/*
+Given 2 ints, a and b, return their sum. However, sums in the range 10..19 inclusive, are forbidden, so in that case just return 20.
+
+
+sortaSum(3, 4) → 7
+sortaSum(9, 4) → 20
+sortaSum(10, 11) → 21
+ */
+
+public class SortaSum {
+
+    public int sortaSum(int a, int b) {
+        if (a + b > 9 && a + b < 20) {
+            return 20;
+        }
+        return a + b;
+    }
+}
