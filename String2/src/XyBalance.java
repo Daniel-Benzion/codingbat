4489609b891307a53ab6ffcb75bd37d65f33c26b
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-01-10 16:11:13 -0500

    xyBalance

---
+++ String2/src/XyBalance.java
@@ -0,0 +1,20 @@
+/*
+We'll say that a String is xy-balanced if for all the 'x' chars in the string, there exists a 'y' char somewhere later in the string. So "xxy" is balanced, but "xyx" is not. One 'y' can balance multiple 'x's. Return true if the given string is xy-balanced.
+
+
+xyBalance("aaxbby") → true
+xyBalance("aaxbb") → false
+xyBalance("yaaxbb") → false
+ */
+public class XyBalance {
+
+    public boolean xyBalance(String str) {
+        boolean result = true;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'x') result = false;
+            if (str.charAt(i) == 'y') result = true;
+        }
+        return result;
+    }
+}
