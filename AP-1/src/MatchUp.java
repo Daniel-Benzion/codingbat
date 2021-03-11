dbbde0df3290ca6b634eb6b3c546da94a94f928b
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-03-11 15:31:41 -0500

    matchUp

---
+++ AP-1/src/MatchUp.java
@@ -0,0 +1,15 @@
+public class MatchUp {
+
+	public int matchUp(String[] a, String[] b) {
+
+		int total = 0;
+
+		for (int i = 0; i < a.length; i++) {
+			if (a[i].isEmpty() || b[i].isEmpty()) continue;
+
+			if (a[i].charAt(0) == b[i].charAt(0)) total++;
+
+		}
+		return total;
+	}
+}
