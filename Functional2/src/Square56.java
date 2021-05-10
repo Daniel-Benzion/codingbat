eb018ebfd16c2699c4e60037eee544c7097bd2b1
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-05-09 21:10:41 -0400

    square56

---
+++ Functional2/src/Square56.java
@@ -0,0 +1,12 @@
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Square56 {
+
+	public List<Integer> square56(List<Integer> nums) {
+		return nums.stream()
+				.map(x -> x * x + 10)
+				.filter(x -> x % 10 != 5 && x % 10 != 6)
+				.collect(Collectors.toList());
+	}
+}
