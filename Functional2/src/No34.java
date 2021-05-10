cfe205c31b392840069c5b110b4fbd552bb450ae
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-05-09 21:09:36 -0400

    no34

--- Functional2/src/No34.java
+++ Functional2/src/No34.java
@@ -3,7 +3,7 @@ import java.util.List;
 public class No34 {
 
 	public List<String> no34(List<String> strings) {
-		strings.removeIf(x -> x.length() == 3 | x.length() == 4);
+		strings.removeIf(x -> x.length() == 3 || x.length() == 4);
 		return strings;
 	}
 }
