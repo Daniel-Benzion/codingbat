bf9269c1b4474884d2159d67698ae1fcec54d5fa
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-05-09 21:08:47 -0400

    two2

--- Functional2/src/Two2.java
+++ Functional2/src/Two2.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Two2 {
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Two2 {
+
+	public List<Integer> two2(List<Integer> nums) {
+		return nums.stream()
+				.map(x -> x * 2)
+				.filter(x -> x % 10 != 2)
+				.collect(Collectors.toList());
+	}
 }
