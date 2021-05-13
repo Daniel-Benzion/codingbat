923f35c1fc06e59d585d78f85da006bba00eeb19
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-05-13 17:09:42 -0400

    bunnyEars2

--- Recursion1/src/BunnyEars2.java
+++ Recursion1/src/BunnyEars2.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class BunnyEars2 {
+public class BunnyEars2 {
+
+	public int bunnyEars2(int bunnies) {
+		if (bunnies == 0) return 0;
+
+		if (bunnies % 2 == 1) return 2 + bunnyEars2(bunnies - 1);
+
+		return 3 + bunnyEars2(bunnies - 1);
+	}
 }
